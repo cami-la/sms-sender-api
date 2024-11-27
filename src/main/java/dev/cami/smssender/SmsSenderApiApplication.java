6d358bd9a4c69cbd58b9919beb87309a3eecfff6
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-11-27 08:43:56 -0300

    :tada: Begin a project
    begin a project

---
+++ src/main/java/dev/cami/smssender/SmsSenderApiApplication.java
@@ -0,0 +1,11 @@
+package dev.cami.smssender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsSenderApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SmsSenderApiApplication.class, args);
+	}
+}
